504af85f2a315a19dd5d0cd5d45310b8d1e095fd
gustbul <122509882+gustbul@users.noreply.github.com>
2023-03-08 22:21:15 +0800

    Update config.ts

--- docs/vitepress/config.ts
+++ docs/vitepress/config.ts
@@ -2,20 +2,26 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/reference/site-config
 export default defineConfig({
-  title: "国信瑞通",
-  description: "国信瑞通 PCDN 服务商",
+  title: '国信瑞通',
+  description: '国信瑞通 PCDN 服务商',
   themeConfig: {
     // https://vitepress.vuejs.org/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: '新用户向导', link: '/guide' }
+            {
+        text: '用户上机',
+        items: [
+          { text: '盒子', link: '/guide/index' },
+          { text: 'x86', link: '/guide/x86' },
+          { text: 'Item C', link: '/item-3' },
+        ],
     ],
 
     sidebar: [
       {
         text: '开始上机',
         items: [
-          { text: '新用户', link: '/guide' },
+          { text: '新用户', link: '/guide/index' },
           { text: 'X86 设备', link: '/guide/x86' }
         ]
       }
